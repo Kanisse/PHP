90eeb62b1779f322755011abcc9edbad62503fc2
Kanisse <73669714+Kanisse@users.noreply.github.com>
2023-05-12 14:50:21 +0100

    init

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div style="display:flex;">
+    <div class="carte">
+        Chiffre d'affaire
+        X  &euro;
+    </div>
+    <div class="carte">
+        Nombre de commandes
+        Y 
+    </div>
+    <div class="carte">
+        Nombre de clients
+        Y 
+    </div>
+    <div class="carte">
+        Nombre de produits
+        Y 
+    </div>
+    <div class="carte">
+        la meilleur vente
+        Y 
+    </div>
+    </div>
+    
+
+    <table>
+        <tr>
+            <th> Nom </th>
+            <th> Prix</th>
+            <th> Categorie</th>
+            <th> action</th>
+        </tr>
+
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
